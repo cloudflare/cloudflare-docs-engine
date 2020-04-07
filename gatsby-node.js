a4b9e29b4a5efca46d66429793bcff56fb322b5f
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-04-07 01:37:44 -0400

    customize webpack to quiet browser logs

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+// https://www.gatsbyjs.org/docs/add-custom-webpack-config/
+exports.onCreateWebpackConfig = ({
+  getConfig,
+  actions,
+  plugins,
+}) => {
+  const config = getConfig()
+
+  // Hides "[HMR] ..." logs in devtools
+  config.entry.commons = config.entry.commons.map(path => (
+    path.indexOf('/webpack-hot-middleware/client.js?') !== -1 ?
+      path : path + '&quiet=true'
+  ))
+
+  actions.replaceWebpackConfig(config)
+
+  actions.setWebpackConfig({
+    plugins: [
+      // Hides React Devtools advertisement in devtools
+      // https://tinyurl.com/hide-react-devtools-advert
+      plugins.define({
+        __REACT_DEVTOOLS_GLOBAL_HOOK__: "({ isDisabled: true })"
+      })
+    ]
+  })
+}
