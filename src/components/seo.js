55e91f232a199158f437fdf6f27c94cf307d8b9e
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-04 18:22:23 -0400

    use large twitter summary card

--- src/components/seo.js
+++ src/components/seo.js
@@ -53,7 +53,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
