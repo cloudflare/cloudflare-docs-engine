09339d926eda009d79af462bf37a603de6fe301c
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-07 21:58:25 -0400

    404 seo

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 import { Link } from "gatsby"
-// import SEO from "../components/seo"
+import SEO from "../components/seo"
 
 import DocsTitle from "../components/docs-title"
 
 const NotFoundPage = () => (
   <>
+    <SEO title="Not found"/>
     <h1>Not found</h1>
     <p>Unfortunately, the page you requested cannot be found.</p>
     <p><Link to="/">Go to <DocsTitle/> docs home</Link></p>
