74ede3d4f7bb915d2bf1d417238d81daa756a201
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-09 00:18:42 -0400

    code block design improvements

--- src/components/mdx/code-block.js
+++ src/components/mdx/code-block.js
@@ -2,52 +2,29 @@ import React from "react"
 
 import Highlight, { defaultProps } from "prism-react-renderer"
 
-const transformations = {
-  js: {
-    keyword: {
-      to: "declaration-keyword",
-      for: ["const", "let", "var", "async", "function", "class"]
-    },
-    punctuation: {
-      to: "operator",
-      for: ["."]
-    },
-    "class-name": {
-      to: "api",
-      for: ["HTMLRewriter", "Request", "Response", "URL", "Error"] // TODO - only applies these to Workers-specific JavaScript blocks
-    },
-    "function": {
-      to: "built-in",
-      for: ["fetch", "console"]
-    }
-  },
-  css: {
-    selector: {
-      to: "operator",
-      for: ["="]
-    },
-    "*": {
-      to: "string",
-      for: (s) => s.match(/^".+"$/)
-    }
+import { transformToken } from "./custom-syntax-highlighting"
+
+const shellLanguages = ["sh", "shell", "bash"]
+
+const addNewlineToEmptyLine = line => {
+  if (line && line.length === 1 && line[0].empty) {
+    // Improves copy/paste behavior
+    line[0].content = "\n"
   }
+
+  return line
 }
 
-const transformToken = ({ token, children, language }) => {
-  const lang = transformations[language]
-  if (!lang) return token
+const getCodeBlockClassName = language => {
+  const base = "CodeBlock CodeBlock-with-rows CodeBlock-scrolls-horizontally"
+  // TODO
+  // return base + " CodeBlock-is-light-in-light-theme"
+  return base + (shellLanguages.includes(language) ? "" : " CodeBlock-is-light-in-light-theme")
+}
 
-  const trans = transformations[language][token]
-  if (!trans) return token
-
-  if (trans.for.includes(children)) {
-    return trans.to
-  }
-
-  const any = transformations[language]["*"]
-  if (any && any.for(children)) return any.to
-
-  return token
+const normalizeLanguage = language => {
+  if (shellLanguages.includes(language)) return "shell"
+  return language
 }
 
 const CodeBlock = props => {
@@ -70,7 +47,7 @@ const CodeBlock = props => {
   return (
     <Highlight {...defaultProps} code={code} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={"CodeBlock CodeBlock-with-rows CodeBlock-is-light-in-light-theme CodeBlock-scrolls-horizontally"} language={language}>
+        <pre className={getCodeBlockClassName(language)} language={language}>
           <code>
             <span className="CodeBlock--line-number-rows" style={{display:"none"}}> {/* TODO - optionally display line numbers */}
               {tokens.map((line, i) => (
@@ -83,7 +60,7 @@ const CodeBlock = props => {
                 <span className="CodeBlock--row" data-line-number={i + 1} key={i}>
                   <span className="CodeBlock--row-indicator"></span>
                   <span className="CodeBlock--row-content">
-                    {line.map((token, key) => (
+                    {addNewlineToEmptyLine(line).map((token, key) => (
                       <span key={key} {...tokenProps(getTokenProps({ token, key }))} />
                     ))}
                   </span>
---
+++ src/components/mdx/custom-syntax-highlighting.js
@@ -0,0 +1,47 @@
+const transformations = {
+  js: {
+    keyword: {
+      to: "declaration-keyword",
+      for: ["const", "let", "var", "async", "function", "class"]
+    },
+    punctuation: {
+      to: "operator",
+      for: ["."]
+    },
+    "class-name": {
+      to: "api",
+      for: ["HTMLRewriter", "Request", "Response", "URL", "Error"] // TODO - only applies these to Workers-specific JavaScript blocks
+    },
+    "function": {
+      to: "built-in",
+      for: ["fetch", "console"]
+    }
+  },
+  css: {
+    selector: {
+      to: "operator",
+      for: ["="]
+    },
+    "*": {
+      to: "string",
+      for: (s) => s.match(/^".+"$/)
+    }
+  }
+}
+
+export const transformToken = ({ token, children, language }) => {
+  const lang = transformations[language]
+  if (!lang) return token
+
+  const trans = transformations[language][token]
+  if (!trans) return token
+
+  if (trans.for.includes(children)) {
+    return trans.to
+  }
+
+  const any = transformations[language]["*"]
+  if (any && any.for(children)) return any.to
+
+  return token
+}
