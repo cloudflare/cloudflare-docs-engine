8d6cba05db21e51e400c78a13d89de76993a00f9
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-12 13:16:31 -0400

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,9 @@ module.exports = {
   },
 
   plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     // Prevent nav from (un)mounting on page navigations
     // https://git.io/JfOKn
     {
@@ -45,7 +48,6 @@ module.exports = {
         path: `${__dirname}/src/content/`
       }
     },
-    `gatsby-plugin-react-helmet`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -53,8 +55,6 @@ module.exports = {
         path: `${__dirname}/src/images/`
       }
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
