e2f6bdda58edde18dc20e5a4c7b347342b1485f9
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-18 23:09:17 -0400

    improve anchor-link useRegularLink criteria

--- src/components/mdx/anchor-link.js
+++ src/components/mdx/anchor-link.js
@@ -4,9 +4,12 @@ import { Link } from "gatsby"
 const linkClassName = "DocsMarkdown--link" // TODO - get prefix from somewhere
 
 export default ({ href, className, ...props }) => {
-  const external = !!href.match(/^https?:/)
+  const isExternal = !!href.match(/^https?:/)
+  const isHash = href.indexOf("#") === 0
 
-  return external ? (
+  const useRegularLink = isExternal || isHash
+
+  return useRegularLink ? (
     <a href={href} className={className || linkClassName} {...props}/>
   ) : (
     <Link to={href} className={className || linkClassName} {...props}/>
