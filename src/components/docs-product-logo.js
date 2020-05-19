dafab95aa3bbd36ba74e8af85515a9fa83742f9b
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-18 23:08:24 -0400

    new logo component pulls path d from gatsby-config

---
+++ src/components/docs-product-logo.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import DocsTitle from "./docs-title"
+import AccessibleSVG from "./accessible-svg"
+
+const ProductLogoPathD = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            cloudflareDocs {
+              productLogoPathD
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return site.siteMetadata.cloudflareDocs.productLogoPathD
+}
+
+const DocsProductLogo = () => (
+  <AccessibleSVG viewBox="0 0 48 48" title={"Cloudflare " + DocsTitle() + " dashboard logomark"}>
+    <path d={ProductLogoPathD()}/>
+  </AccessibleSVG>
+)
+
+export default DocsProductLogo
