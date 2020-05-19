e8cadf786c5c26d0589ab78315e0cbc8c13c467f
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-19 05:18:59 -0400

    remove /* TODO */ comments

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import AnchorLink from "../components/mdx/anchor-link" // TODO
+import AnchorLink from "../components/mdx/anchor-link"
 import SEO from "../components/seo"
 
 import DocsTitle from "../components/docs-title"
@@ -10,7 +10,6 @@ const NotFoundPage = () => (
     <SEO title="Not found"/>
     <h1>Not found</h1>
     <p>Unfortunately, the page you requested cannot be found.</p>
-    {/* TODO - get className from somewhere? */}
     <p><AnchorLink href="/">Go to <DocsTitle/> docs home</AnchorLink></p>
   </>
 )
