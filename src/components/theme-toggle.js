2526edd0113b49e3272f0f9c6f148de36a3074ea
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-20 12:08:34 -0400

    a11y toggle theme icons

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -2,6 +2,7 @@ import React from "react"
 import Helmet from "react-helmet"
 
 import setIntervalVisible from "set-interval-visible"
+import AccessibleSVG from "./accessible-svg"
 
 const colorSchemeQuery = "(prefers-color-scheme: dark)"
 
@@ -140,20 +141,14 @@ class ThemeToggle extends React.Component {
           <label className="ThemeToggle--toggle" htmlFor="ThemeToggle">
             <div className="ThemeToggle--toggle-handle"></div>
             <div className="ThemeToggle--toggle-handle-icon ThemeToggle--sun">
-              <svg id="SunIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" role="img" fill="currentColor">
-                <defs>
-                  <title>Sun icon</title>
-                </defs>
+              <AccessibleSVG viewBox="0 0 16 16" title="Light theme icon (depiction of a sun)">
                 <path d="M7.5 11.465a3.482 3.482 0 01-1.596-.662L4.11 12.596a.5.5 0 01-.707-.707l1.793-1.793A3.482 3.482 0 014.535 8.5H2a.5.5 0 010-1h2.535a3.482 3.482 0 01.662-1.596L3.404 4.11a.5.5 0 01.707-.707l1.793 1.793A3.482 3.482 0 017.5 4.535V2a.5.5 0 011 0v2.535a3.482 3.482 0 011.596.662l1.793-1.793a.5.5 0 01.707.707l-1.793 1.793c.343.458.577 1.003.662 1.596H14a.5.5 0 110 1h-2.535a3.482 3.482 0 01-.662 1.596l1.793 1.793a.5.5 0 01-.707.707l-1.793-1.793a3.482 3.482 0 01-1.596.662V14a.5.5 0 11-1 0v-2.535z"/>
-              </svg>
+              </AccessibleSVG>
             </div>
             <div className="ThemeToggle--toggle-handle-icon ThemeToggle--moon">
-              <svg id="MoonIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" role="img" fill="currentColor">
-                <defs>
-                  <title>Moon icon</title>
-                </defs>
+              <AccessibleSVG viewBox="0 0 16 16" title="Dark theme icon (depiction of a moon)">
                 <path d="M7.067 3.087a5 5 0 005.466 7.026 5 5 0 11-5.466-7.026z"/>
-              </svg>
+              </AccessibleSVG>
             </div>
           </label>
         </div>
