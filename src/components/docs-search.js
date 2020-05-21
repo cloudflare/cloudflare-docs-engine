3ec1db9976e4fd0b7eaa8e70c65e4e457fd00a62
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-21 15:42:12 -0400

    use accessible icon component for search icon

--- src/components/docs-search.js
+++ src/components/docs-search.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react"
-
 import { navigate } from "@reach/router"
 
 import Helmet from "react-helmet"
+
 import DocsTitle from "./docs-title"
+import AccessibleSVG from "./accessible-svg"
 
 const DocsSearch = () => {
   useEffect(() => {
@@ -78,13 +79,9 @@ const DocsSearch = () => {
         <div className="DocsSearch--input-wrapper">
           <input id="DocsSearch--input" className="DocsSearch--input" type="text" spellCheck="false" autoComplete="false" placeholder={"Search " + DocsTitle() + " docs..."}/>
           <div className="DocsSearch--input-icon">
-            <svg id="SearchIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" role="img" aria-labelledby="SearchIcon--title SearchIcon--desc" fill="currentColor">
-              <defs>
-                <title id="SearchIcon--title">Search icon</title>
-                <desc id="SearchIcon--desc">A magnifying glass used to illustrate the concept of search.</desc>
-              </defs>
+            <AccessibleSVG viewBox="0 0 16 16" title="Search icon (depiction of a magnifying glass)">
               <path d="M11.999 10.585l3.458 3.458a1 1 0 01-1.414 1.414L10.585 12a6.5 6.5 0 111.414-1.414zM6.75 11.5a4.75 4.75 0 100-9.5 4.75 4.75 0 000 9.5z"/>
-            </svg>
+            </AccessibleSVG>
           </div>
           <div className="DocsSearch--input-bottom-border"></div>
         </div>
