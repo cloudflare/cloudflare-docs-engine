c157d6cd9bb0b188a5c60174fe2849f9f1ccf3a3
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-21 18:30:30 -0400

    impement focus-group on dropdown component

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,38 +1,88 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import createFocusGroup from "focus-group"
+
 class Dropdown extends React.Component {
 
   constructor(props) {
     super(props)
 
-    this.ref = React.createRef()
+    this.container = React.createRef()
+    this.button = React.createRef()
 
     this.state = {
       expanded: false
     }
+
+    this.onExpandButtonClick = this.onExpandButtonClick.bind(this)
+    this.handleClickOutside = this.handleClickOutside.bind(this)
+    this.handleEscapeKey = this.handleEscapeKey.bind(this)
   }
 
   onExpandButtonClick() {
-    const { expanded } = this.state
-    this.setState({ expanded: !expanded })
+    this.setState(state => {
+      const expanded = !state.expanded
+
+      if (expanded) {
+        this.focusGroup.focusNodeAtIndex(0)
+      }
+
+      return { expanded }
+    })
   }
 
   handleClickOutside(event) {
-    if (this.state.expanded && this.ref.current && !this.ref.current.contains(event.target)) {
+    if (this.state.expanded && this.container.current && !this.container.current.contains(event.target)) {
       this.setState({
         expanded: false
       })
     }
   }
 
+  handleEscapeKey(event) {
+    if (event.key !== "Escape") return
+
+    if (this.button.current) {
+      this.button.current.focus()
+    }
+
+    this.setState({ expanded: false })
+  }
+
   componentDidMount() {
-    // TODO - implement focus/blur behavior
-    document.addEventListener("click", this.handleClickOutside.bind(this))
+    const el = this.container.current
+    const members = el.querySelectorAll(this.props.focusGroupSelector)
+
+    this.focusGroup = createFocusGroup({
+      members: members,
+      wrap: true,
+      stringSearch: true,
+      keybindings: {
+        next: [{ keyCode: 40 }, { keyCode: 39 }],
+        prev: [{ keyCode: 38 }, { keyCode: 37 }],
+        first: { keyCode: 74, metaKey: true },
+        last: { keyCode: 75, metaKey: true }
+      }
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevState.expanded && this.state.expanded) {
+      this.focusGroup.activate()
+      document.addEventListener("click", this.handleClickOutside)
+      document.addEventListener("keyup", this.handleEscapeKey)
+    }
+
+    if (prevState.expanded && !this.state.expanded) {
+      this.focusGroup.deactivate()
+      document.removeEventListener("click", this.handleClickOutside)
+      document.removeEventListener("keyup", this.handleEscapeKey)
+    }
   }
 
   componentWillUnmount() {
-    document.removeEventListener("click", this.handleClickOutside.bind(this))
+    this.focusGroup.deactivate()
   }
 
   render() {
@@ -43,10 +93,11 @@ class Dropdown extends React.Component {
     buttonClassName += (className ? " " : "") + "Button"
 
     return (
-      <div className={className} ref={this.ref} data-expanded={expanded}>
+      <div className={className} ref={this.container} data-expanded={expanded}>
         <button
+          ref={this.button}
           className={buttonClassName}
-          onClick={this.onExpandButtonClick.bind(this)}
+          onClick={this.onExpandButtonClick}
           aria-expanded={expanded}
           children={this.props.buttonChildren}
         />
@@ -55,6 +106,10 @@ class Dropdown extends React.Component {
       </div>
     )
   }
+}
+
+Dropdown.defaultProps = {
+  focusGroupSelector: "a[href]"
 }
 
 export default Dropdown
