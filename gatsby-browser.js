5f90b149914cde2b8ce725510330c083e4cbcaac
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-21 20:16:29 -0400

    remove unused css, clean up gasby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,10 @@
 // See: https://www.gatsbyjs.org/docs/browser-apis/
 
-// TODO - load these from components themselves?
+// Required until wider support
+// https://caniuse.com/#search=focus-visible
+import "focus-visible-polyfill"
 
-// CSS
-
+// TODO - move these to @cloudflare/ui or similar
 import "./src/css/ui/global/box-sizing.css"
 import "./src/css/ui/global/element-normalization.css"
 import "./src/css/ui/global/sizes-variables.css"
@@ -19,6 +20,7 @@ import "./src/css/ui/helpers/is-smooth-scrolling.css"
 import "./src/css/ui/helpers/is-visually-hidden.css"
 import "./src/css/ui/helpers/with-styled-webkit-scrollbars.css"
 
+// TODO - should these be imported by their respective components?
 import "./src/css/ui/components/css-transition-fade-lift.css"
 import "./src/css/ui/components/aspect-ratio.css"
 import "./src/css/ui/components/breadcrumbs.css"
@@ -27,35 +29,26 @@ import "./src/css/ui/components/cloudflare-logo.css"
 import "./src/css/ui/components/cloudflare-workers-logo.css"
 import "./src/css/ui/components/code-block.css"
 import "./src/css/ui/components/theme-toggle.css"
-// import "./src/css/ui/components/unordered-list.css" // TODO
-import "./src/css/ui/components/definition-list.css"
 import "./src/css/ui/components/dropdown.css"
-import "./src/css/ui/components/error-page.css"
-import "./src/css/ui/components/footer.css"
 import "./src/css/ui/components/inline-code.css"
 import "./src/css/ui/components/link.css"
-import "./src/css/ui/components/markdown-lite.css"
 import "./src/css/ui/components/network-map.css"
-import "./src/css/ui/components/number.css"
-import "./src/css/ui/components/radio-tabs.css"
 import "./src/css/ui/components/scrollbars.css"
 import "./src/css/ui/components/stream-video.css"
 import "./src/css/ui/components/superscript.css"
 
+// TODO - should these be imported by their respective components?
 import "./src/css/docs/components/docs-noscript.css"
+import "./src/css/docs/components/docs-nav-logo-lockup.css"
+import "./src/css/docs/components/docs-page.css"
+import "./src/css/docs/components/docs-sidebar.css"
 import "./src/css/docs/components/docs-body.css"
+import "./src/css/docs/components/docs-table-of-contents.css"
 import "./src/css/docs/components/docs-content.css"
 import "./src/css/docs/components/docs-markdown.css"
+import "./src/css/docs/components/docs-toolbar.css"
+import "./src/css/docs/components/docs-search.css"
 import "./src/css/docs/components/docs-mobile-header.css"
 import "./src/css/docs/components/docs-mobile-nav.css"
 import "./src/css/docs/components/docs-mobile-title-header.css"
-import "./src/css/docs/components/docs-nav-logo-lockup.css"
-import "./src/css/docs/components/docs-page.css"
-import "./src/css/docs/components/docs-search.css"
-import "./src/css/docs/components/docs-sidebar.css"
-import "./src/css/docs/components/docs-table-of-contents.css"
-import "./src/css/docs/components/docs-toolbar.css"
 import "./src/css/docs/components/docs-tutorials.css"
-
-// JS
-import "focus-visible-polyfill"
