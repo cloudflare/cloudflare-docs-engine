864e78d52e1c4327b703f8a176064e29743b7d9f
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-05-21 20:52:45 -0400

    theme toggle keyboard shortcut (⇧+D), and tooltip

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -4,6 +4,7 @@ import Helmet from "react-helmet"
 import setIntervalVisible from "set-interval-visible"
 import AccessibleSVG from "./accessible-svg"
 
+const keyboardShortcutKey = "D"
 const colorSchemeQuery = "(prefers-color-scheme: dark)"
 
 const storeInterval = 30 * 1000
@@ -81,9 +82,10 @@ class ThemeToggle extends React.Component {
 
     this.onCheckboxChange = this.onCheckboxChange.bind(this)
     this.onMediaMatchChange = this.onMediaMatchChange.bind(this)
+    this.handleKeyboardCommand = this.handleKeyboardCommand.bind(this)
   }
 
-  onCheckboxChange(event) {
+  toggle() {
     this.setState(state => {
       const checked = !state.checked
 
@@ -95,12 +97,23 @@ class ThemeToggle extends React.Component {
     })
   }
 
+  onCheckboxChange() {
+    this.toggle()
+  }
+
   onMediaMatchChange(event) {
     const theme = event.matches ? "dark" : "light"
     setTheme(theme)
 
     const checked = theme === "dark"
     this.setState({ checked })
+  }
+
+  handleKeyboardCommand(event) {
+    if (event.key !== keyboardShortcutKey) return
+    if (!event.shiftKey) return
+
+    this.toggle()
   }
 
   componentDidMount() {
@@ -116,11 +129,15 @@ class ThemeToggle extends React.Component {
     this.interval = setIntervalVisible(() => {
       storeTheme(getTheme())
     }, storeInterval)
+
+    document.addEventListener("keydown", this.handleKeyboardCommand)
   }
 
   componentWillUnmount() {
     this.query.removeListener(this.onMediaMatchChange)
     this.interval.clear()
+
+    document.removeEventListener("keydown", this.handleKeyboardCommand)
   }
 
   render() {
@@ -133,27 +150,33 @@ class ThemeToggle extends React.Component {
           <script>{ getThemeFromStorageSource }</script>
         </Helmet>
 
-        <div className="ThemeToggle" data-is-loading={loading}>
-          <input
-            type="checkbox"
-            id="ThemeToggle"
-            className="ThemeToggle--input"
-            onChange={this.onCheckboxChange}
-            checked={this.state.checked} />
+        <div className="Tooltip---root">
+          <div className="ThemeToggle" data-is-loading={loading}>
+            <input
+              type="checkbox"
+              id="ThemeToggle"
+              className="ThemeToggle--input"
+              onChange={this.onCheckboxChange}
+              checked={this.state.checked} />
 
-          <label className="ThemeToggle--toggle" htmlFor="ThemeToggle">
-            <div className="ThemeToggle--toggle-handle"></div>
-            <div className="ThemeToggle--toggle-handle-icon ThemeToggle--sun">
-              <AccessibleSVG title="Light theme icon (depiction of a sun)" viewBox="0 0 16 16">
-                <path d="M7.5 11.465a3.482 3.482 0 01-1.596-.662L4.11 12.596a.5.5 0 01-.707-.707l1.793-1.793A3.482 3.482 0 014.535 8.5H2a.5.5 0 010-1h2.535a3.482 3.482 0 01.662-1.596L3.404 4.11a.5.5 0 01.707-.707l1.793 1.793A3.482 3.482 0 017.5 4.535V2a.5.5 0 011 0v2.535a3.482 3.482 0 011.596.662l1.793-1.793a.5.5 0 01.707.707l-1.793 1.793c.343.458.577 1.003.662 1.596H14a.5.5 0 110 1h-2.535a3.482 3.482 0 01-.662 1.596l1.793 1.793a.5.5 0 01-.707.707l-1.793-1.793a3.482 3.482 0 01-1.596.662V14a.5.5 0 11-1 0v-2.535z"/>
-              </AccessibleSVG>
-            </div>
-            <div className="ThemeToggle--toggle-handle-icon ThemeToggle--moon">
-              <AccessibleSVG title="Dark theme icon (depiction of a moon)" viewBox="0 0 16 16">
-                <path d="M7.067 3.087a5 5 0 005.466 7.026 5 5 0 11-5.466-7.026z"/>
-              </AccessibleSVG>
-            </div>
-          </label>
+            <label className="ThemeToggle--toggle" htmlFor="ThemeToggle">
+              <div className="ThemeToggle--toggle-handle"></div>
+              <div className="ThemeToggle--toggle-handle-icon ThemeToggle--sun">
+                <AccessibleSVG title="Light theme icon (depiction of a sun)" viewBox="0 0 16 16">
+                  <path d="M7.5 11.465a3.482 3.482 0 01-1.596-.662L4.11 12.596a.5.5 0 01-.707-.707l1.793-1.793A3.482 3.482 0 014.535 8.5H2a.5.5 0 010-1h2.535a3.482 3.482 0 01.662-1.596L3.404 4.11a.5.5 0 01.707-.707l1.793 1.793A3.482 3.482 0 017.5 4.535V2a.5.5 0 011 0v2.535a3.482 3.482 0 011.596.662l1.793-1.793a.5.5 0 01.707.707l-1.793 1.793c.343.458.577 1.003.662 1.596H14a.5.5 0 110 1h-2.535a3.482 3.482 0 01-.662 1.596l1.793 1.793a.5.5 0 01-.707.707l-1.793-1.793a3.482 3.482 0 01-1.596.662V14a.5.5 0 11-1 0v-2.535z"/>
+                </AccessibleSVG>
+              </div>
+              <div className="ThemeToggle--toggle-handle-icon ThemeToggle--moon">
+                <AccessibleSVG title="Dark theme icon (depiction of a moon)" viewBox="0 0 16 16">
+                  <path d="M7.067 3.087a5 5 0 005.466 7.026 5 5 0 11-5.466-7.026z"/>
+                </AccessibleSVG>
+              </div>
+            </label>
+
+            <span className="Tooltip" role="tooltip" position="left">
+              {`Set theme to ${this.state.checked ? "light" : "dark"} (⇧+D)`}
+            </span>
+          </div>
         </div>
       </>
     )
