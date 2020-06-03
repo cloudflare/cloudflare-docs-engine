c41b1845e477b1ce8696f111b65956ad405ed4ec
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-06-03 17:49:35 -0400

    fix aria role attr name

--- src/components/mdx/aside.js
+++ src/components/mdx/aside.js
@@ -3,7 +3,7 @@ import React from "react"
 import { className } from "./root"
 
 const Aside = props => (
-  <aside className={className("aside")} aria-role="note" data-type={props.type}>
+  <aside className={className("aside")} role="note" data-type={props.type}>
     {props.header && (
       <div className={className("aside-header")}>
         {props.header}
