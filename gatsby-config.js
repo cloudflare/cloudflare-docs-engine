f5cda45748aa3678092f37d1166125eaa609c5d0
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-06-02 23:28:22 -0400

    better defaults for manifest data

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,11 +72,11 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "cloudflare-docs",
+        short_name: "docs",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "#f38020",
+        theme_color: "#f38020",
         display: "minimal-ui",
         icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
       }
