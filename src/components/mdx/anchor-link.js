5ce512094b7d07282c120c1599c54d21e7d329a0
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-06-18 18:24:57 -0400

    add icon for external links automatically

--- src/components/mdx/anchor-link.js
+++ src/components/mdx/anchor-link.js
@@ -3,16 +3,31 @@ import { Link } from "gatsby"
 
 import { className } from "./root"
 const linkClassName = className("link")
+const contentClassName = `${ linkClassName }-content`
+const externalIconClassName = `${linkClassName}-external-icon`
 
-export default ({ href, className, ...props }) => {
+import ExternalLinkIcon from "../icons/external-link"
+
+export default ({ href, className, children, ...props }) => {
   const isExternal = !!href.match(/^https?:/)
   const isHash = href.indexOf("#") === 0
 
   const useRegularLink = isExternal || isHash
 
   return useRegularLink ? (
-    <a href={href} className={className || linkClassName} {...props}/>
+    isExternal ? (
+      <a href={href} className={className || linkClassName} {...props}>
+        <span className={contentClassName}>{children}</span>
+        <ExternalLinkIcon className={externalIconClassName}/>
+      </a>
+    ) : (
+      <a href={href} className={className || linkClassName} {...props}>
+        <span className={contentClassName}>{children}</span>
+      </a>
+    )
   ) : (
-    <Link to={href} className={className || linkClassName} {...props}/>
+    <Link to={href} className={className || linkClassName} {...props}>
+      <span className={contentClassName}>{children}</span>
+    </Link>
   )
 }
