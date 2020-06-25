42357eee76d159ebd4d75c1e1b29dde96800cf2b
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-06-25 14:19:37 -0400

    smooth browser resizing

---
+++ src/components/browser-resize-tracking.js
@@ -0,0 +1,41 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const attr = "is-resizing"
+
+class SmoothBrowserResizing extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.handleWindowResize = this.handleWindowResize.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleWindowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize)
+  }
+
+  handleWindowResize() {
+    clearTimeout(this.resizeTimeout)
+
+    document.documentElement.setAttribute(attr, "")
+
+    this.resizeTimeout = setTimeout(() => {
+      document.documentElement.removeAttribute(attr)
+    }, this.props.debounce)
+  }
+
+  render() {
+    return null
+  }
+}
+
+SmoothBrowserResizing.defaultProps = {
+  debounce: 400
+}
+
+export default SmoothBrowserResizing
