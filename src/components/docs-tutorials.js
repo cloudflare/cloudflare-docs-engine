cloudflare/cloudflare-docs-engine

commit ef5235d0728b9d887561c9452143efe29024fc01

improve tutorials overview on mobile (hide "ago")

src/components/docs-tutorials.js
@@ -88,7 +88,11 @@ const DocsTutorials = () => {
               </Link>
             </div>
             <div className="DocsTutorials--column" data-column="updated">
-              <TimeAgo date={tutorial.updated} minPeriod={60}/>
+              <TimeAgo date={tutorial.updated} formatter={(value, unit) => (
+                <React.Fragment>
+                  {value} {unit}<span className="DocsTutorials--ago-text"> ago</span>
+                </React.Fragment>
+              )} minPeriod={60}/>
             </div>
             <div className="DocsTutorials--column" data-column="difficulty">{tutorial.difficulty}</div>
             <div className="DocsTutorials--column" data-column="length">

src/css/docs/components/docs-tutorials.css
@@ -96,6 +96,10 @@
   .DocsTutorials--link {
     font-size: 1em;
   }
+
+  .DocsTutorials--ago-text {
+    display: none;
+  }
 }
 
 [js-focus-visible-polyfill-available] .DocsTutorials--link:focus {

commit 48eef16c762cd7554c8a70a94b4a8b92747f3cb6

fix pluralization on tutorials listing
regression caused by:
https://github.com/cloudflare/workers-docs-engine/commit/ef5235d0728b9d887561c9452143efe29024fc01#diff-7f8b97a960593654b395f4d4e7617b4f

src/components/docs-tutorials.js
@@ -88,7 +88,7 @@ const DocsTutorials = () => {
             <div className="DocsTutorials--column" data-column="updated">
               <TimeAgo date={tutorial.updated} formatter={(value, unit) => (
                 <React.Fragment>
-                  {value} {unit}<span className="DocsTutorials--ago-text"> ago</span>
+                  {value} {unit}{value > 1 ? "s" : ""}<span className="DocsTutorials--ago-text"> ago</span>
                 </React.Fragment>
               )} minPeriod={60}/>
             </div>