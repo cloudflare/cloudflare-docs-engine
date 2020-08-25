b861843c391576ee83ceaa496be2ddaaa172e7f7
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-08-24 20:56:04 -0400

    attempt to fix breadcrumbs not showing up #195

--- src/utils/get-breadcrumbs.js
+++ src/utils/get-breadcrumbs.js
@@ -1,13 +1,21 @@
 import getPageTitle from "./get-page-title"
+import getPathPrefix from "./get-path-prefix"
 import getParentPath from "./get-parent-path"
 import getPageByPath from "./get-page-by-path"
 import getNormalizedPath from "./get-normalized-path"
 
+const pathPrefix = getPathPrefix()
+
 const getBreadcrumbs = (pages, location) => {
   let out = []
 
+  const pathname =
+    location.pathname.startsWith(pathPrefix) ?
+      location.pathname.substr(pathPrefix.length) :
+      location.pathname
+
   try {
-    const page = getPageByPath(pages, location.pathname)
+    const page = getPageByPath(pages, pathname)
     let parent = getPageByPath(pages, getParentPath(page.fields.slug))
 
     while (parent) {
