bbac1857d3c10739ee87567ad1a49a5a36a9e185
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-10-29 14:00:40 -0400

    fix #300: broken analytics.js during local dev

--- src/html.js
+++ src/html.js
@@ -10,7 +10,7 @@ export default function HTML(props) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         { props.headComponents }
-        <script type="text/javascript" src="/_cf/analytics.js" />
+        <script type="text/javascript" src="https://developers.cloudflare.com/_cf/analytics.js" />
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: googleAnalytics }}/>
       </head>
 
