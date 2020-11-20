17041bbd300bea296c06df27c4b8584c00858c16
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-11-20 12:45:27 -0500

    support docs icon config by both key and pathD

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,67 @@ const docsConfig = require("./docs-config.js")
 
 const isProduction = process.env.NODE_ENV === "production"
 
+const getProduct = (name) => {
+  const repo = "@cloudflare/cloudflare-brand-assets"
+  const dir = "resources/product-icons/"
+  return `./node_modules/${repo}/${dir}/${name}.js`
+}
+
+const products = [
+  "1.1.1.1",
+  "access",
+  "analytics",
+  "api",
+  "argo-tunnel",
+  "byoip",
+  "cloudflare-for-teams",
+  "cloudflare-one",
+  "distributed-web",
+  "docs-engine",
+  "events",
+  "firewall",
+  "gateway",
+  "http3",
+  "images",
+  "internet",
+  "load-balancing",
+  "logs",
+  "magic-transit",
+  "mobile-sdk",
+  "network-interconnect",
+  "randomness-beacon",
+  "registrar",
+  "spectrum",
+  "ssl",
+  "stream",
+  "tenant",
+  "terraform",
+  "time-services",
+  "waf",
+  "warp-client",
+]
+
+const productIcons = {}
+products.forEach(name => {
+  productIcons[name] = require(getProduct(name)).pathD
+})
+
+if (docsConfig.productLogoPathD && docsConfig.productIconKey) {
+  return Error("Set either `productLogoPathD` or `productIconKey` in docs-config.js, not both")
+}
+
+if (docsConfig.productIconKey) {
+  docsConfig.productLogoPathD = productIcons[docsConfig.productIconKey]
+}
+
 const siteMetadata = docsConfig.siteMetadata
 siteMetadata.cloudflareDocs = {}
 Object.keys(docsConfig).forEach(prop => {
   if (prop === "siteMetadata") return
   siteMetadata.cloudflareDocs[prop] = docsConfig[prop]
 })
+
+docsConfig.cloudflareDocs.productIcons = productIcons
 
 // We exposed friendlier siteMetadata.url to Docs consumers but
 // gatsby-plugin-sitemap requires `siteUrl` https://git.io/JUUxW
