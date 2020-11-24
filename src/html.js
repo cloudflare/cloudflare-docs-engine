ea2031c9575f7785ec9356963fd9cf6e00d2c379
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-11-23 20:06:54 -0500

    cleanup

--- src/html.js
+++ src/html.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import googleAnalytics from './utils/google-analytics'
+import googleAnalytics from "./utils/google-analytics"
 
 export default function HTML(props) {
   return (
