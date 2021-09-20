379a4d31818f38c0ace2cd5b64ee4c3e5897ab05
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2021-09-20 16:38:55 -0500

    Part 1 of updating to use new way of getting and displaying icons (#354)
    * Part 1 of updating to use new way of getting and displaying icons
    
    * support old pathD in config
    
    Co-authored-by: ganchimeg <ganchimeg@cloudflare.com>

--- src/components/docs-product-logo.js
+++ src/components/docs-product-logo.js
@@ -2,12 +2,18 @@ import React from "react"
 import getCloudflareDocsConfig from "../utils/get-cloudflare-docs-config"
 import AccessibleSVG from "./accessible-svg"
 
-export default () => {
-  const { product, productLogoPathD }  = getCloudflareDocsConfig()
 
-  return (
+export default () => {
+  const { product, productLogoPathD, productIconKey, logoSVGContent } = getCloudflareDocsConfig()
+
+  return logoSVGContent ? (
+    <span
+      title={`Cloudflare ${productIconKey ? productIconKey : 'product'} logo`}
+      dangerouslySetInnerHTML={{ __html: logoSVGContent }}
+    />
+  ) : (
     <AccessibleSVG title={`Cloudflare ${product} logo`} viewBox="0 0 48 48">
-      <path d={productLogoPathD}/>
+      <path d={productLogoPathD} />
     </AccessibleSVG>
   )
 }
