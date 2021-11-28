e4bf04337ab5f763f5819cf9ef13482b408c8809
Temitayo Fadojutimi <temitayo.fadojutimi@cloudflare.com>
2021-11-28 12:29:34 +0100

    fix(canonical-url): remove gatsby-plugin-canonical-urls dependency and implement adding canonical tags manually
     fixes https://github.com/cloudflare/cloudflare-docs-engine/pull/365

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,6 @@
+import React from "react";
+import url from "url";
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
@@ -5,3 +8,26 @@
  */
 
 // You can delete this file if you're not using it
+
+const SITE_URL = 'https://developers.cloudflare.com';
+
+exports.onRenderBody = (
+  { setHeadComponents, pathname = `/` },
+) => {
+  const siteUrl = SITE_URL.replace(/\/$/, ``)
+  const parsed = url.parse(`${siteUrl}${pathname}`)
+
+  parsed.pathname = !parsed.pathname.endsWith('/') ? '/' : parsed.pathname;
+
+  const pageUrl = `${parsed.protocol}//${parsed.host}${parsed.pathname}`;
+
+  setHeadComponents([
+    <link
+      rel="canonical"
+      key={pageUrl}
+      href={pageUrl}
+      data-baseprotocol={parsed.protocol}
+      data-basehost={parsed.host}
+    />,
+  ])
+}
